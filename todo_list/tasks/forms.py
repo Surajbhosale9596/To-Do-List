aa4d142501a585174b9418a511f24ab686ed27fd
Surajbhosale9596 <101340005+Surajbhosale9596@users.noreply.github.com>
2022-07-19 11:45:12 +0530

    Add files via upload

---
+++ todo_list/tasks/forms.py
@@ -0,0 +1,13 @@
+from dataclasses import fields
+from pyexpat import model
+from django import forms
+from django.forms import ModelForm
+
+from .models import *
+
+class TaskForm(forms.ModelForm):
+    title= forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Add new task...'}))
+
+    class Meta:
+        model=Task
+        fields='__all__'
